fbc38b316c7a2a90654a5f638804544b678b4e2b
insightprotocol <61179865+insightprotocol@users.noreply.github.com>
2020-03-10 02:00:36 +0900

    Create deploy_contracts.js

---
+++ migrations/deploy_contracts.js
@@ -0,0 +1,5 @@
+var InsightProtocol = artifacts.require("./InsightProtocol.sol");
+
+module.exports = function(deployer, network, accounts) {
+  deployer.deploy(InsightProtocol, 'INX', 'InsightProtocol', accounts[0]);
+};
